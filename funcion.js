24eab324c997355ba0ea21ac4f5e91466315f24d
Rodrigo Díaz <rooloo97@gmail.com>
2022-11-29 19:46:09 -0300

    agregue js

---
+++ funcion.js
@@ -0,0 +1,38 @@
+const alumno1 = {
+    edad: 20,
+    sexo: `M`
+}
+
+const alumno2 = {
+    edad: 22,
+    sexo: `M`
+}
+
+const alumno3 = {
+    edad: 10,
+    sexo: `M`
+}
+
+const alumno4 = {
+    edad: 20,
+    sexo: `F`
+}
+
+const alumno5 = {
+    edad: 20,
+    sexo: `F`
+}
+
+const listaDeAlumnos = [alumno1, alumno2, alumno3, alumno4, alumno5];
+
+const promedioHombres = (listaDeAlumnos) => {
+    let suma = 0;
+    let i = 0
+    listaDeAlumnos.forEach((alumno) => {
+        if(alumno.sexo === `M`) {
+            suma += alumno.edad;
+            i++;
+        }
+    });
+    return suma / i;
+}
